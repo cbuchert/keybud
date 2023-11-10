f3e39fc8eac1987240682fbaf280c24feb08ff40
Chris <christianbuchert@gmail.com>
2023-11-10 15:35:06 -0700

    getActiveCodes includes the keyboard event code.

--- src/utils/getActiveCodes.test.ts
+++ src/utils/getActiveCodes.test.ts
@@ -1,11 +1,44 @@
 import { describe, expect, it } from "vitest"
 import { getActiveCodes } from "./getActiveCodes.ts"
 
+class TestKeyboardEvent extends Event {
+  code: string
+  ctrlKey: boolean
+  altKey: boolean
+  shiftKey: boolean
+  metaKey: boolean
+
+  constructor(
+    _: string,
+    init: {
+      code: string
+      ctrlKey?: boolean
+      altKey?: boolean
+      shiftKey?: boolean
+      metaKey?: boolean
+    }
+  ) {
+    super("KeyboardEvent")
+    this.code = init.code
+    this.ctrlKey = init.ctrlKey ?? false
+    this.altKey = init.altKey ?? false
+    this.shiftKey = init.shiftKey ?? false
+    this.metaKey = init.metaKey ?? false
+  }
+}
+
 describe("getActiveCodes", () => {
   it("returns an empty set when no keyboard event and no pinned keys are passed", () => {
     const event = null
     const pinnedKeys = new Set<KeyboardEvent["code"]>()
 
     expect(getActiveCodes(event, pinnedKeys)).toEqual(new Set())
+  })
+
+  it("returns a set with the keyboard event code when no pinned keys are passed", () => {
+    const event = new TestKeyboardEvent("keydown", { code: "KeyA" })
+    const pinnedKeys = new Set<KeyboardEvent["code"]>()
+
+    expect(getActiveCodes(event, pinnedKeys)).toEqual(new Set(["KeyA"]))
   })
 })
