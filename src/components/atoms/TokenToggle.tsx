19d893271bfd0a4f791b7e1e767a438b31b0b8c2
Chris <christianbuchert@gmail.com>
2024-01-08 13:12:20 -0700

    `TokenToggle` has a checkbox to indicate checkeddness.

--- src/components/atoms/TokenToggle.tsx
+++ src/components/atoms/TokenToggle.tsx
@@ -12,11 +12,12 @@ export const TokenToggle: FC<Props> = ({ children, isChecked, onChange }) => {
     <button
       type={"button"}
       className={classNames(
-        "rounded-full px-4 py-1 transition-all",
+        "rounded-full px-4 py-1 transition-all flex gap-3 items-center",
         isChecked ? "bg-blue-600 text-white" : "bg-gray-300 text-gray-800"
       )}
       onClick={onChange}
     >
+      <span className={"w-2.5"}>{isChecked ? "☑" : "☐"}</span>
       {children}
     </button>
   )
