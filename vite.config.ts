57705472277d9de70ba8d25dd83f09fc7bef853f
chris <christianbuchert@gmail.com>
2025-05-27 17:40:28 -0600

    fixing deps.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
-import react from "@vitejs/plugin-react"
+import reactSWC from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite(), reactSWC()],
   test: {
     environment: "jsdom",
   },
